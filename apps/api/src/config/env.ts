ed0e4da188be634bec5b8de4d790c8185bdc999d
Ben Nguyen <jl.jimluv@gmail.com>
2026-06-05 10:35:39 +0700

    feat: add Prisma client helper

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 4000;
+const DATABASE_URL = process.env.DATABASE_URL || "";
+const NODE_ENV = "development";
 
 export const env = {
   PORT,
+  DATABASE_URL,
+  NODE_ENV,
 };
